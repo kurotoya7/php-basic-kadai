57633e74a0c6069165cad1c9a70defab76cc28c5
kurotoya7 <161889834+kurotoya7@users.noreply.github.com>
2024-06-08 14:18:15 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>PHP基礎編</title>
+ </head>
+ 
+ <body>
+ <?php   
+ $food = ['名前'=>'玉ねぎ','値段'=>200,'産地'=>'北海道',];
+     
+   foreach($food as $index => $value){
+     echo"{$index}:{$value}<br>";
+   }
+  ?>
+ </body>
+ 
+ </html>
